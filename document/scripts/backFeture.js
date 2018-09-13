d205188dc37761d91118db9e671a00c569922745
Yuval <yuval@korentec.co.il>
2018-09-13 10:35:58 +0300

    back btn feature - initial code

--- document/scripts/backFeture.js
+++ document/scripts/backFeture.js
@@ -1,3 +1,20 @@
 /* ************ back feature script for BADAH document ************* */
 
 'use strict'
+
+/* ----------------------- DOM manipulation ------------------------ */
+
+// back button creation
+const backBtn = create('button',['button', 'actionButton'], null, 'Back')
+
+// back button on click event
+backBtn.addEventListener('click', () => {
+  if (!location.hash) {
+    alert('You are already on the home page')
+    return
+  }
+
+  // TBD: back feature on click code
+})
+
+append(container, backBtn)
