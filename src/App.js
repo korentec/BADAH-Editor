1a53541c57a323f7aba692e91400044f562951c0
Yuval <yuval@korentec.co.il>
2018-09-17 17:20:37 +0300

    accessing electron from the react app - example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
 
+// accessing electron from the react app - example
+// const electron = window.require('electron')
+// const fs = electron.remote.require('fs')
+// const ipcRenderer  = electron.ipcRenderer
+
 class App extends Component {
   render() {
     return (
